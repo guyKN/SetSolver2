c54decd77e9561944e947f1c5310ca6d1014dd7c
Guy Knaan <guyknaan@gmail.com>
2020-10-13 17:32:54 +0300

    Make ImageProcessingThread send DrawbleOnCanvas rather than file paths

--- app/src/main/java/com/guykn/setsolver/ImageProcessingThreadManager.java
+++ app/src/main/java/com/guykn/setsolver/ImageProcessingThreadManager.java
@@ -7,14 +7,16 @@ import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import com.guykn.setsolver.drawing.DrawableOnCanvas;
+import com.guykn.setsolver.drawing.RotatedRectangleList;
 import com.guykn.setsolver.imageprocessing.detect.ContourBasedCardDetector;
-import com.guykn.setsolver.set.SetCard;
 
 import org.opencv.core.Mat;
 
 import java.io.IOException;
 
 public class ImageProcessingThreadManager {
+    //todo: move the handler into this class, make it implement an interface, and make MainActivity Implement an interface that this calls.
     public static String TAG = "ImageProcessingThreadManager";
     public interface MessageConstants{
         int MESSAGE_SUCCESS=0;
@@ -61,19 +63,10 @@ public class ImageProcessingThreadManager {
         public void run(){
             try {
                 ContourBasedCardDetector cardDetector = new ContourBasedCardDetector(imagePath, config, context);
-                Mat drawing = cardDetector.getContourBoxDrawing(ContourBasedCardDetector.BackgroundType.ORIGINAL_IMAGE);
-                SetCard card = cardDetector.getCardClassifications();
-
-                //saves the image
-                ImageFileManager imageFileManager = new ImageFileManager(context);
-                imageFileManager.createTempImage();
-                imageFileManager.saveImage(drawing);
+                RotatedRectangleList cardLocations = cardDetector.getAllCardRectangles();
 
                 DisplayImageMessage message = new DisplayImageMessage();
-                message.imagePath = imageFileManager.getCurrentImagePath();
-                message.stringToDisplay = card.toString();
-
-
+                message.drawable = cardLocations;
                 handler.obtainMessage(MessageConstants.MESSAGE_SUCCESS, message).sendToTarget(); //sends the location of the current image and its classification to the UI thread
             }catch (IOException e){
                 e.printStackTrace();
@@ -81,12 +74,22 @@ public class ImageProcessingThreadManager {
             }
         }
 
+        @Deprecated
+        public String saveImage(Mat drawing) throws IOException {
+            ImageFileManager imageFileManager = new ImageFileManager(context);
+            imageFileManager.createTempImage();
+            imageFileManager.saveImage(drawing);
+            return imageFileManager.getCurrentImagePath();
+
+        }
+
+
 
     }
     public static class DisplayImageMessage{
         @Nullable
-        public String imagePath = null;
-        @Nullable
         public String stringToDisplay = null;
+        @Nullable
+        public DrawableOnCanvas drawable = null;
     }
 }
--- app/src/main/java/com/guykn/setsolver/drawing/DrawableList.java
+++ app/src/main/java/com/guykn/setsolver/drawing/DrawableList.java
@@ -6,6 +6,8 @@ import android.graphics.Paint;
 import java.util.List;
 
 public abstract class DrawableList<Drawable extends DrawableOnCanvas> implements DrawableOnCanvas{
+    private static final int DEFAULT_TRIM_SIZE = 30;
+
     protected List<Drawable> drawables;
 
     protected void addDrawable(Drawable drawable){
@@ -17,5 +19,22 @@ public abstract class DrawableList<Drawable extends DrawableOnCanvas> implements
         for(Drawable drawable: drawables){
             drawable.drawOnCanvas(canvas, paint);
         }
+    }
+
+    /**
+     * Cuts off elements at the end of the list until it is of length maxSize.
+     * Used in order to restrict the size of the list in order to avoid the awful lag that may come from high amounts of drawables.
+     * Does nothing if the Size of the list is less than maxSize
+     * @param maxSize the maximum length that the list is allowed to be.
+     */
+    public void trimToSize(int maxSize){
+        int size = drawables.size();
+        if(size <= maxSize){
+            return;
+        }
+        drawables.subList(maxSize, size).clear();
+    }
+    public void trim(){
+        trimToSize(DEFAULT_TRIM_SIZE);
     }
 }
